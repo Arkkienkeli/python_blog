56ad8f418e65e42826cda50a6219e884e9732708
Nick <nikitam851@gmail.com>
2014-07-07 23:04:40 +0400

    5th

--- blog/urls.py
+++ blog/urls.py
@@ -4,29 +4,33 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('blog.views.index',
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('blog.views.index',
     # Examples:
     # url(r'^$', 'blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'index', name='index'),
-
-    url(r'^admin/', include(admin.site.urls)),
 )
 urlpatterns += patterns('blog.views.posts',
     # Examples:
     # url(r'^$', 'blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^posts/$', 'posts_list', name='posts'),
-    url(r'^posts/([\w\-]+)-(?P<post_id>\d+)/$', 'blog.views.post_detail', name='post_detail'),
-    url(r'^posts/(\d+)/(\d+)/(\d+)/$', 'blog.views.posts_by_date', name='posts_by_date'),
-    url(r'^posts/category/(\w+)-(\d+)/$', 'blog.views.posts_by_category', name='posts_by_category'),
-    url(r'^posts/tag/(\w+)/$', 'blog.views.posts_by_tag', name='posts_by_tag'),
-    url(r'^posts/page-(\d+)/$', 'blog.views.posts_list', name='posts_list'),
-    url(r'^posts/(\w+)-(\d+)/edit/$', 'blog.views.post_edit', name='post_edit'),
+    url(r'^posts/([\w\-]+)-(?P<post_id>\d+)/$', 'post_detail', name='post_detail'),
+    url(r'^posts/(\d+)/(\d+)/(\d+)/$', 'posts_by_date', name='posts_by_date'),
+    url(r'^posts/(\d+)/(\d+)/$', 'posts_by_date', name='posts_by_date'),
+    url(r'^posts/(\d+)/$', 'posts_by_date', name='posts_by_date'),
+    url(r'^posts/category/(\w+)-(\d+)/$', 'posts_by_category', name='posts_by_category'),
+    url(r'^posts/tag/(\w+)/$', 'posts_by_tag', name='posts_by_tag'),
+    url(r'^posts/page-(\d+)/$', 'posts_list', name='posts_list'),
+    url(r'^posts/(\w+)-(\d+)/edit/$', 'post_edit', name='post_edit'),
 
 )
 urlpatterns += patterns('blog.views.users',
 
-    url(r'^user/(?P<user_id>\d+)/$', 'blog.views.user_profile', name='profile'),
-    url(r'^user/(?P<user_id>\d+)/edit$', 'blog.views.user_profile_edit', name='user_profile_edit'),
+    url(r'^user/(?P<user_id>\d+)/$', 'user_profile', name='profile'),
+    url(r'^user/(?P<user_id>\d+)/edit$', 'user_profile_edit', name='user_profile_edit'),
 
 )
--- blog/views/posts.py
+++ blog/views/posts.py
@@ -1,5 +1,8 @@
 #coding=utf-8
+from django.http.response import Http404
+from datetime import date
 
+from django.db.models import Q
 from django.shortcuts import render_to_response
 from blog.models import Post
 
@@ -13,17 +16,47 @@ def post_detail(request, post_id):
 	return render_to_response('post_detail.html', {'post':post})
 
 
-def posts_by_date(request, post_date):
-    pass
+def posts_by_date(request, year, month=None, day=None):
+
+    #import pdb; pdb.set_trace()
+
+    year = int (year)
+    if month:
+        month = int(month)
+    if day:
+        day = int(day)
+    
+    if year<1900:
+        raise Http404
+    if month and (month<1 or month>12):
+        raise Http404
+    if day and (day<1 or day>31):
+        raise Http404
+
+    q = [Q(date__year=year)]
+    if month:
+        q.append(Q(date__month = month))
+    if day:
+        q.append(Q(date__day = day))
+
+    query = q.pop()
+    for item in q:
+        query |= item
+    dat = date(year,month or 1,day or 1)
+    posts = Post.objects.filter(query)
+    print dat
+    context = {'dat':dat,'posts':posts}
+    print year, month, day
+    return render_to_response('posts_by_date.html', context)
+
 
 def posts_by_category(request, category_id):
     pass
 
+
 def posts_by_tag(request, tag):
     pass
 
-def posts_list(request):
-    pass
 
 def post_edit(request, post):
     pass
